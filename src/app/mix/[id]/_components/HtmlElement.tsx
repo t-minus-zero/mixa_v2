b1524a9feffe97f21f8032972597549a1d7fc03e
t-minus-zero <sirgurumain@gmail.com>
2025-03-17 03:21:18 +0000

    aCan dded color cue selection on html tree

--- src/app/mix/[id]/_components/HtmlElement.tsx
+++ src/app/mix/[id]/_components/HtmlElement.tsx
@@ -125,10 +125,11 @@ function HtmlElement({ node, level = 0, children }) {
     <li
       ref={elementRef}
       className={`
-        ${node.childrens && node.childrens.length > 0 && isOpen ? 'border-zinc-300' : 'border-transparent'}
+        ${node.childrens && node.childrens.length > 0 && isOpen ? 'border-blue-400' : 'border-blue-400'}
         ${selectionParent && selectionParent.id === node.id ? 'border-l bg-zinc-100/50 rounded-lg' : ''}
+        ${selection.id === node.id ? 'border-blue-400 bg-blue-50/50 rounded-lg' : 'border-transparent'}
         ${getDropIndicatorStyle()}
-        relative py-1
+        relative border
       `}
       style={{ marginLeft: level / 5 + "rem" }}
       draggable="true"
@@ -142,9 +143,9 @@ function HtmlElement({ node, level = 0, children }) {
           selectionHandler(node);
         }}
         className={`
-          ${selection.id === node.id ? 'bg-zinc-50/50 border-blue-500' : 'border-transparent'}
+          ${selection.id === node.id ? 'bg-blue-200/50' : ''}
           ${draggedItem?.id === node.id ? 'opacity-50' : ''}
-          tracking-tight relative group w-full h-full flex flex-row items-center rounded-lg flex-start border 
+          tracking-tight relative group w-full h-full flex flex-row items-center rounded-lg flex-start border border-transparent
         `}
       >
         <button
